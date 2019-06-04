9193ac39b84d759c55cc72d795fed2e1fa1edf27
David Lin <haiflin@iu.edu>
2019-06-04 15:59:03 -0400

    http part end

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgurBuilder';
 
 class App extends Component {
-  state = {
-    show: true
-  };
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false});
-    },5000);
-  }
+  
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
+          <BurgerBuilder />
         </Layout>
       </div>
     );
