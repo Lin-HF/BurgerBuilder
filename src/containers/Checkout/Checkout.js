bf61ec996563994818d7f3e4dbbb2e5de426d3b3
David Lin <haiflin@iu.edu>
2019-06-07 17:53:39 -0400

    query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
+
     state ={
         ingredients : {
             salad: 1,
@@ -10,6 +11,16 @@ class Checkout extends Component {
             bacon: 1
         }
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        } 
+        this.setState({ingredients: ingredients});   
+    }
+
     checkoutCanceldHandler = () => {
         this.props.history.goBack();
     }
