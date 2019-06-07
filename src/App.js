b69a419c576c140e53689494ba1ce991e436531b
David Lin <haiflin@iu.edu>
2019-06-07 19:24:37 -0400

    Order pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgurBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import {Route, Switch} from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   
@@ -14,7 +15,9 @@ class App extends Component {
           <Checkout /> */}
           <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path='/orders' component={Orders} />
           <Route path="/" exact component={BurgerBuilder} />
+          
           </Switch>
           {/* <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} /> */}
