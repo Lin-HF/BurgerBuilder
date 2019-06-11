78c858a77fef4eaed13a8d34a31ee0e3bec41e0c
David Lin <haiflin@iu.edu>
2019-06-10 20:57:31 -0400

    bug fixed

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,11 +83,9 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: '',
-                validation: {
-                    required: false
-                },
-                valid: false,
+                value: 'fastest',
+                validation: {},
+                valid: true,
                 touched: false
             }
         },
@@ -120,6 +118,10 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true;
+
+        if (!rules) {
+            return true;
+        }
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
