690692e963568ec147e34c04d398da24cb31cba7
David Lin <haiflin@iu.edu>
2019-06-15 16:17:25 -0400

    install redux

---
+++ src/store/reducer.js
@@ -0,0 +1,15 @@
+import * as actionType from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+    }
+    return state;
+}
+
+export default reducer;
