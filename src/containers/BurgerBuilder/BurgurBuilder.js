6be928cbaa078d0f68483099bc9b9df7aa691d30
David Lin <haiflin@iu.edu>
2019-06-16 14:05:42 -0400

    Checkout and Contact with Redux

--- src/containers/BurgerBuilder/BurgurBuilder.js
+++ src/containers/BurgerBuilder/BurgurBuilder.js
@@ -106,16 +106,17 @@ class BurgerBuilder extends Component {
         // }).catch(response => {
         //     this.setState({ loading: false, purchasing: false });
         // });
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.props.price);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+        // const queryParams = [];
+        // for (let i in this.state.ingredients) {
+        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        // }
+        // queryParams.push('price=' + this.props.price);
+        // const queryString = queryParams.join('&');
+        // this.props.history.push({
+        //     pathname: '/checkout',
+        //     search: '?' + queryString
+        // });
+        this.props.history.push('/checkout');
     }
 
     render() {
