2d9793edd54963479e1aaa7fb6599bb90452f6a0
David Lin <haiflin@iu.edu>
2019-06-17 16:43:29 -0400

    fetch data asynchronously

--- src/containers/BurgerBuilder/BurgurBuilder.js
+++ src/containers/BurgerBuilder/BurgurBuilder.js
@@ -22,8 +22,8 @@ class BurgerBuilder extends Component {
         // totalPrice: 4,
         // purchasable: false,
         purchasing: false,
-        loading: false,
-        error: false
+        // loading: false,
+        // error: false
     }
 
     componentDidMount() {
@@ -149,9 +149,9 @@ class BurgerBuilder extends Component {
             />
         }
 
-        if (this.state.loading) {
-            orderSummary = <Spinner />
-        }
+        // if (this.state.loading) {
+        //     orderSummary = <Spinner />
+        // }
 
         return (
             <Aux>
