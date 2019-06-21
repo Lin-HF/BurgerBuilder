0ee23f0ca37771d2bfd5c0cebb9e947f7e05ed68
David Lin <haiflin@iu.edu>
2019-06-21 10:58:20 -0400

    guarding Routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgurBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
 
 class App extends Component {
@@ -14,23 +14,48 @@ class App extends Component {
     this.props.onTryAutoSignup();
   }
   render() {
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/"/>
+      </Switch>
+
+    );
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/logout' component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/"/>
+        </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
+          {routes}
           {/* <BurgerBuilder />
           <Checkout /> */}
-          <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
-          <Route path='/logout' component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {/* <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path='/orders' component={Orders} />
+            <Route path='/auth' component={Auth} />
+            <Route path='/logout' component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch> */}
           {/* <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} /> */}
         </Layout>
       </div>
     );
+  }
+}
+const mapstateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -40,4 +65,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapstateToProps, mapDispatchToProps)(App));
