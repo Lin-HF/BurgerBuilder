8f893e4fa0fa5d720eb9f156e11781ff9684bdc9
David Lin <haiflin@iu.edu>
2019-06-25 22:37:42 -0400

    container test with bug

--- src/containers/BurgerBuilder/BurgurBuilder.js
+++ src/containers/BurgerBuilder/BurgurBuilder.js
@@ -12,7 +12,8 @@ import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
 
 
-class BurgerBuilder extends Component {
+// class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
