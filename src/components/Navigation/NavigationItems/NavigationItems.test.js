526ae8f86e3f6280da685ce6606924c57cbf1547
David Lin <haiflin@iu.edu>
2019-06-25 22:19:26 -0400

    [test] contains

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -19,4 +19,8 @@ describe('<NavigationItems />',() => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+    it('should render 3 <NavigationItems /> if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
